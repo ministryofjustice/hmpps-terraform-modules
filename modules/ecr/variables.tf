f713b9c7650c241590e253f15490e4bf8bab49df
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-05-30 17:33:45 +0100

    Added ECS cluster module

---
+++ modules/ecr/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {}
+variable "project" {}
+variable "app_name" {}
+variable "environment" {}
+
+variable "business_unit" {
+  description = "The name of our business unit, i.e. development."
+}
+
+variable "role_arn" {}
