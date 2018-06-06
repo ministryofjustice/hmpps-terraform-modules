59cf44acbe50e32beb427d53fe3f395dd6c26774
smjmoj <smjmoj@gmail.com>
2018-06-06 12:19:45 +0100

    Updated internetgateway module to use tags map

--- modules/internetgateway/variables.tf
+++ modules/internetgateway/variables.tf
@@ -1,19 +1,7 @@
-variable "region" {
-  description = "The AWS region."
-}
-
-variable "environment" {
-  description = "The name of our environment"
-}
-
-variable "project" {
-  description = "The name of our project"
-}
-
 variable "vpc_id" {}
 
-variable "business_unit" {
-  description = "The name of our business unit, i.e. development."
-}
-
 variable "gateway_name" {}
+
+variable "tags" {
+  type = "map"
+}
