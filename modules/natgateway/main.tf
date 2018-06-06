434b41e4c2e0e042d4bc13bf2b5d0ee481133f58
smjmoj <smjmoj@gmail.com>
2018-06-06 12:29:37 +0100

    Added tags map to module

--- modules/natgateway/main.tf
+++ modules/natgateway/main.tf
@@ -1,22 +1,10 @@
 resource "aws_eip" "environment" {
-  vpc = true
-
-  tags {
-    Name          = "tf-${var.az}-nat-gateway-eip"
-    Project       = "${var.project}"
-    Environment   = "${var.environment}"
-    Business-Unit = "${var.business_unit}"
-  }
+  vpc  = true
+  tags = "${merge(var.tags, map("name", "${var.az}-nat-gateway-eip"))}"
 }
 
 resource "aws_nat_gateway" "environment" {
   allocation_id = "${aws_eip.environment.id}"
   subnet_id     = "${var.subnet}"
-
-  tags {
-    Name          = "tf-${var.az}-nat-gateway"
-    Project       = "${var.project}"
-    Environment   = "${var.environment}"
-    Business-Unit = "${var.business_unit}"
-  }
+  tags          = "${merge(var.tags, map("name", "${var.az}-nat-gateway"))}"
 }
