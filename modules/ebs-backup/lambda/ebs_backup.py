f656f773971391dd86b880052296c5eea9c9a62a
Brett Minnie <github@bdb-studios.co.uk>
2018-06-15 14:45:12 +0100

    Snapshot on volume, not instance

--- modules/ebs-backup/lambda/ebs_backup.py
+++ modules/ebs-backup/lambda/ebs_backup.py
@@ -10,7 +10,6 @@ config.read('./vars.ini')
 
 
 def lambda_handler(event, context):
-
     regions = config.get('regions', 'regionList')
     region_list = regions.split(',')
     instance_tag = config.get('main', 'ec2_instance_tag')
@@ -21,49 +20,42 @@ def lambda_handler(event, context):
         print("Checking Region %s" % aws_region)
         account = event['account']
         ec = boto3.client('ec2', region_name=aws_region)
-        reservations = ec.describe_instances()['Reservations']
+        volumes = ec.describe_volumes()['Volumes']
 
-        instances = sum(
-            [
-                [i for i in r['Instances']]
-                for r in reservations
-                ], [])
-
-        for instance in instances:
-            for device in instance['BlockDeviceMappings']:
+        for volume in volumes:
+            if 'Attachments' in volume:
                 backup_device = False
-                if device.get('Ebs', None) is None:
-                    # skip non EBS volumes
-                    continue
-                vol_id = device['Ebs']['VolumeId']
-                instance_id=instance['InstanceId']
-                instance_name = ''
+                instance_name = 'Unknown'
 
-                for tags in instance['Tags']:
-                    if tags["Key"] == 'Name':
-                        instance_name = tags["Value"]
-                    if tags["Key"] == 'CreateSnapshot':
-                        backup_device = True
+                vol_id = volume['VolumeId']
+                instance_id = volume['Attachments'][0]['InstanceId']
+                device_name = volume['Attachments'][0]['Device']
 
-                if backup_device:
-                    print("Found EBS Volume %s on Instance %s, creating Snapshot" % (vol_id, instance['InstanceId']))
-                    snap = ec.create_snapshot(
-                        Description="Snapshot of Instance %s (%s) %s" % (instance_id, instance_name, device['DeviceName']),
-                        VolumeId=vol_id,
-                    )
-                    snapshot = "%s_%s" % (snap['Description'], str(datetime.date.today()))
-                    delete_date = datetime.date.today() + datetime.timedelta(days=retention_days)
-                    delete_fmt = delete_date.strftime('%Y-%m-%d')
-                    ec.create_tags(
-                        Resources=[snap['SnapshotId']],
-                        Tags=[
-                            {'Key': 'DeleteOn', 'Value': delete_fmt},
-                            {'Key': 'Name', 'Value': snapshot},
-                            {'Key': 'InstanceId', 'Value': instance_id},
-                            {'Key': 'InstanceName', 'Value': instance_name},
-                            {'Key': 'DeviceName', 'Value': device['DeviceName']}
-                        ]
-                    )
+                if 'Tags' in volume:
+                    for tags in volume['Tags']:
+                        if tags["Key"] == 'Name':
+                            instance_name = tags["Value"]
+                        if tags["Key"] == 'CreateSnapshot':
+                            backup_device = True
+
+                    if backup_device:
+                        print("Found EBS Volume %s on Instance %s, creating Snapshot" % (vol_id, instance_id))
+                        snap = ec.create_snapshot(
+                            Description="Snapshot of Instance %s (%s) %s" % (instance_id, instance_name, device_name),
+                            VolumeId=vol_id,
+                        )
+                        snapshot = "%s_%s" % (snap['Description'], str(datetime.date.today()))
+                        delete_date = datetime.date.today() + datetime.timedelta(days=retention_days)
+                        delete_fmt = delete_date.strftime('%Y-%m-%d')
+                        ec.create_tags(
+                            Resources=[snap['SnapshotId']],
+                            Tags=[
+                                {'Key': 'DeleteOn', 'Value': delete_fmt},
+                                {'Key': 'Name', 'Value': snapshot},
+                                {'Key': 'InstanceId', 'Value': instance_id},
+                                {'Key': 'DeviceName', 'Value': device_name}
+                            ]
+                        )
 
         delete_on = datetime.date.today().strftime('%Y-%m-%d')
         filters = [
