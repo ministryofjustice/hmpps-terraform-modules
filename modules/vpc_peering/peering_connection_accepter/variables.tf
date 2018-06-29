d65a0582ec9a5468dbf68ea6fd7519e30bcff231
Don Nyambudzi <Don.Nyambudzi@madiro.co.uk>
2018-06-29 09:51:36 +0100

    VPC peering modules done (#32)

---
+++ modules/vpc_peering/peering_connection_accepter/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_peering_connection_id" {}
+
+variable "auto_accept" {}
+
+variable "peering_connection_name" {}
+
+variable "tags" {
+  type = "map"
+}
---
+++ modules/vpc_peering/peering_connection_requester/main.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc_peering_connection" "environment" {
+  peer_owner_id = "${var.peer_owner_id}"
+  peer_vpc_id   = "${var.peer_vpc_id}"
+  vpc_id        = "${var.current_vpc_id}"
+  auto_accept   = "${var.auto_accept}"
+
+  accepter {
+    allow_remote_vpc_dns_resolution = "${var.dns_resolution}"
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = "${var.dns_resolution}"
+  }
+
+  tags = "${merge(var.tags, map("Name", "${var.peering_connection_name}-peer-connection"))}"
+}
