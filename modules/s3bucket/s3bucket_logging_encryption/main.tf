3095dfbe5149a403e1ce369627447aa26bb68175
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-07-04 11:46:55 +0100

    Added s3bucket module with logging and encryption

---
+++ modules/s3bucket/s3bucket_logging_encryption/main.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket" "environment" {
+  bucket = "${var.s3_bucket_name}-s3bucket"
+  acl    = "${var.acl}"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  logging {
+    target_bucket = "${var.target_bucket}"
+    target_prefix = "${var.target_prefix}"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${var.kms_master_key_id}"
+        sse_algorithm     = "${var.sse_algorithm}"
+      }
+    }
+  }
+
+  tags = "${merge(var.tags, map("Name", "${var.s3_bucket_name}-s3-bucket"))}"
+}
