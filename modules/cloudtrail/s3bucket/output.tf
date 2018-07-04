8974443fcdf564117fa6a3e2498158239b57c460
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-07-04 13:51:23 +0100

    Added cloudtrail for specific s3bucket

---
+++ modules/cloudtrail/s3bucket/output.tf
@@ -0,0 +1,7 @@
+output "cloudtrail_arn" {
+  value = "${aws_cloudtrail.environment.arn}"
+}
+
+output "cloudtrail_id" {
+  value = "${aws_cloudtrail.environment.id}"
+}
---
+++ modules/cloudtrail/s3bucket/variables.tf
@@ -0,0 +1,19 @@
+variable "s3_bucket_name" {}
+
+variable "globalevents" {}
+
+variable "cloudtrailname" {}
+
+variable "multiregion" {}
+
+variable "tags" {
+  type = "map"
+}
+
+variable "enable_logging" {
+  default = true
+}
+
+variable "s3_bucket_arn" {
+  description = "S3bucket to log"
+}
