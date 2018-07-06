936cf17199411ef4d247fab3e4f96eedf66e1c03
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-07-06 14:10:56 +0100

    Added availability zone output

--- modules/subnets/output.tf
+++ modules/subnets/output.tf
@@ -5,3 +5,7 @@ output "subnetid" {
 output "routetableid" {
   value = "${aws_route_table.environment.id}"
 }
+
+output "availability_zone" {
+  value = "${aws_subnet.environment.availability_zone}"
+}
