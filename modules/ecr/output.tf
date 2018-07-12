b5eabdf88bec05de3bee91296e35b674f0c8cb05
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-07-12 18:47:26 +0100

    ECR outputs updated and policy now fed in

--- modules/ecr/output.tf
+++ modules/ecr/output.tf
@@ -5,3 +5,7 @@ output "repo_repository_url" {
 output "repo_repository_arn" {
   value = "${aws_ecr_repository.repo.arn}"
 }
+
+output "repo_repository_name" {
+  value = "${aws_ecr_repository.repo.name}"
+}
--- modules/ecr/variables.tf
+++ modules/ecr/variables.tf
@@ -1,3 +1,3 @@
 variable "app_name" {}
 
-variable "role_arn" {}
+variable "policy" {}
