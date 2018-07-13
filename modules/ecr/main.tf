05fd6adb71e82fefca0aa414ffbd143b2fe37807
Don Nyambudzi <Don.Nyambudzi@madiro.co.uk>
2018-07-13 09:14:37 +0100

    ECR outputs updated and policy now fed in (#44)

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -6,37 +6,6 @@ resource "aws_ecr_repository" "repo" {
 
 resource "aws_ecr_repository_policy" "repo" {
   repository = "${aws_ecr_repository.repo.name}"
-
-  policy = <<EOF
-{
-    "Version": "2008-10-17",
-    "Statement": [
-        {
-            "Sid": "new policy",
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": "${var.role_arn}"
-            },
-            "Action": [
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:BatchGetImage",
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:PutImage",
-                "ecr:InitiateLayerUpload",
-                "ecr:UploadLayerPart",
-                "ecr:CompleteLayerUpload",
-                "ecr:DescribeRepositories",
-                "ecr:GetRepositoryPolicy",
-                "ecr:ListImages",
-                "ecr:DeleteRepository",
-                "ecr:BatchDeleteImage",
-                "ecr:SetRepositoryPolicy",
-                "ecr:DeleteRepositoryPolicy"
-            ]
-        }
-    ]
-  }
-EOF
-
+  policy     = "${var.policy}"
   depends_on = ["aws_ecr_repository.repo"]
 }
