bcee40f9cb86670130016d939aa67c44626391f4
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-07-17 17:11:42 +0100

    Added db instance module

--- modules/rds/db_instance/main.tf
+++ modules/rds/db_instance/main.tf
@@ -1,19 +1,19 @@
-resource "aws_iam_role" "enhanced_monitoring" {
-  count = "${var.create_monitoring_role ? 1 : 0}"
+# resource "aws_iam_role" "enhanced_monitoring" {
+#   count = "${var.create_monitoring_role ? 1 : 0}"
 
-  name               = "${var.monitoring_role_name}"
-  assume_role_policy = "${file("${path.module}/policy/enhancedmonitoring.json")}"
-}
+#   name               = "${var.monitoring_role_name}"
+#   assume_role_policy = "${file("${path.module}/policy/enhancedmonitoring.json")}"
+# }
 
-resource "aws_iam_role_policy_attachment" "enhanced_monitoring" {
-  count = "${var.create_monitoring_role ? 1 : 0}"
+# resource "aws_iam_role_policy_attachment" "enhanced_monitoring" {
+#   count = "${var.create_monitoring_role ? 1 : 0}"
 
-  role       = "${aws_iam_role.enhanced_monitoring.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
-}
+#   role       = "${aws_iam_role.enhanced_monitoring.name}"
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+# }
 
 resource "aws_db_instance" "this" {
-  count = "${var.create && !local.is_mssql ? 1 : 0}"
+  # count = "${var.create && !local.is_mssql ? 1 : 0}"
 
   identifier = "${var.identifier}"
 
@@ -46,7 +46,7 @@ resource "aws_db_instance" "this" {
   iops                = "${var.iops}"
   publicly_accessible = "${var.publicly_accessible}"
   monitoring_interval = "${var.monitoring_interval}"
-  monitoring_role_arn = "${coalesce(var.monitoring_role_arn, join("", aws_iam_role.enhanced_monitoring.*.arn))}"
+  monitoring_role_arn = "${var.monitoring_role_arn}"
 
   allow_major_version_upgrade = "${var.allow_major_version_upgrade}"
   auto_minor_version_upgrade  = "${var.auto_minor_version_upgrade}"
