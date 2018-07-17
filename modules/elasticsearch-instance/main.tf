076cfd3bb17a8232e6d997a2242e880417cc5a19
Brett Minnie <github@bdb-studios.co.uk>
2018-07-17 11:02:58 +0100

    Opinionated ES instance module

---
+++ modules/elasticsearch-instance/main.tf
@@ -0,0 +1,40 @@
+module "create_elasticsearch_instance" {
+  source                      = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//ec2"
+  app_name                    = "${var.app_name}"
+  ami_id                      = "${var.ami_id}"
+  instance_type               = "${var.instance_type}"
+  subnet_id                   = "${var.subnet_id}"
+  iam_instance_profile        = "${var.instance_profile}"
+  associate_public_ip_address = false
+  monitoring                  = true
+  user_data                   = "${var.user_data}"
+  CreateSnapshot              = true
+  tags                        = "${var.instance_tags}"
+  key_name                    = "${var.ssh_deployer_key}"
+  vpc_security_group_ids      = "${var.security_groups}"
+}
+
+module "create_elasticsearch_ebs_volume" {
+  source            = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//ebs//ebs_volume"
+  CreateSnapshot    = true
+  tags              = "${var.volume_tags}"
+  availability_zone = "${var.volume_availability_zone}"
+  volume_size       = "${var.volume_size}"
+  encrypted         = true
+  app_name          = "${var.environment_identifier}-elasticsearch-volume"
+}
+
+module "attach_elasticsearch_1_ebs_volume" {
+  source      = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//ebs//ebs_attachment"
+  device_name = "/dev/xvdb"
+  instance_id = "${module.create_elasticsearch_instance.instance_id}"
+  volume_id   = "${module.create_elasticsearch_ebs_volume.id}"
+}
+
+resource "aws_route53_record" "internal_elasticsearch_dns" {
+  name    = "elasticsearch-1.${var.zone_name}"
+  type    = "A"
+  zone_id = "${var.zone_id}"
+  ttl     = 300
+  records = ["${module.create_elasticsearch_instance.private_ip}"]
+}
---
+++ modules/elasticsearch-instance/variables.tf
@@ -0,0 +1,40 @@
+# Instance variables
+
+variable "app_name" {}
+
+variable "environment_identifier" {}
+
+variable "ami_id" {}
+
+variable "instance_type" {}
+
+variable "subnet_id" {}
+
+variable "instance_profile" {}
+
+variable "user_data" {}
+
+variable "instance_tags" {
+  type = "map"
+}
+
+variable "ssh_deployer_key" {}
+
+variable "security_groups" {
+  type = "list"
+}
+
+# Ebs volume variables
+variable "volume_tags" {
+  type = "map"
+}
+
+variable "volume_availability_zone" {}
+
+variable "volume_size" {}
+
+#Route53
+variable "zone_name" {}
+
+variable "zone_id" {}
+
