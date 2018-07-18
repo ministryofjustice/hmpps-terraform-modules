ab19bbe5eb3e4088ae7b212d69e2de2578ae73ab
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-07-18 12:49:42 +0100

    --no-edit

--- modules/rds/db_instance/main.tf
+++ modules/rds/db_instance/main.tf
@@ -1,17 +1,3 @@
-# resource "aws_iam_role" "enhanced_monitoring" {
-#   count = "${var.create_monitoring_role ? 1 : 0}"
-
-#   name               = "${var.monitoring_role_name}"
-#   assume_role_policy = "${file("${path.module}/policy/enhancedmonitoring.json")}"
-# }
-
-# resource "aws_iam_role_policy_attachment" "enhanced_monitoring" {
-#   count = "${var.create_monitoring_role ? 1 : 0}"
-
-#   role       = "${aws_iam_role.enhanced_monitoring.name}"
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
-# }
-
 resource "aws_db_instance" "this" {
   # count = "${var.create && !local.is_mssql ? 1 : 0}"
 
