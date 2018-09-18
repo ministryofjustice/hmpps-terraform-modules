51153795ac07a3076b9cd93e9e3a8877c2fcb2cd
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-09-18 14:00:39 +0100

    Removed alfresco ssh deployer key

--- projects/alfresco/common/main.tf
+++ projects/alfresco/common/main.tf
@@ -82,35 +82,6 @@ module "s3alb_logs_policy" {
   policyfile   = "${data.template_file.s3alb_logs_policy.rendered}"
 }
 
-############################################
-# DEPLOYER KEY FOR PROVISIONING
-############################################
-
-module "ssh_key" {
-  source   = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=pre-shared-vpc//modules//ssh_key"
-  keyname  = "${local.common_name}"
-  rsa_bits = "4096"
-}
-
-# Add to SSM
-module "create_parameter_ssh_key_private" {
-  source         = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=pre-shared-vpc//modules//ssm//parameter_store_file"
-  parameter_name = "${local.common_name}-ssh-private-key"
-  description    = "${local.common_name}-ssh-private-key"
-  type           = "SecureString"
-  value          = "${module.ssh_key.private_key_pem}"
-  tags           = "${local.tags}"
-}
-
-module "create_parameter_ssh_key" {
-  source         = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=pre-shared-vpc//modules//ssm//parameter_store_file"
-  parameter_name = "${local.common_name}-ssh-public-key"
-  description    = "${local.common_name}-ssh-public-key"
-  type           = "String"
-  value          = "${module.ssh_key.public_key_openssh}"
-  tags           = "${local.tags}"
-}
-
 ###############################################
 # RDS DB PASSWORD
 ###############################################
--- projects/alfresco/common/output.tf
+++ projects/alfresco/common/output.tf
@@ -19,20 +19,6 @@ output "common_s3_lb_logs_bucket" {
   value = "${module.s3_lb_logs_bucket.s3_bucket_name}"
 }
 
-# SSH KEY
-output "common_ssh_deployer_key" {
-  value = "${module.ssh_key.deployer_key}"
-}
-
-## AWS PARAMETER STORE
-output "common_ssm_ssh_private_key_name" {
-  value = "${module.create_parameter_ssh_key_private.name}"
-}
-
-output "common_ssm_ssh_public_key_name" {
-  value = "${module.create_parameter_ssh_key.name}"
-}
-
 # ENVIRONMENTS SETTINGS
 # tags
 output "common_tags" {
