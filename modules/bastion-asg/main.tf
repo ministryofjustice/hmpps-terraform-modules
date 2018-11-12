313b21bddc5b8b4317b49f6ee28beb93a2bad57a
Brett Minnie <github@bdb-studios.co.uk>
2018-11-12 14:53:36 +0000

    Removed TG config

--- modules/bastion-asg/main.tf
+++ modules/bastion-asg/main.tf
@@ -1,20 +1,6 @@
-terraform {
-  # The configuration for this backend will be filled in by Terragrunt
-  backend "s3" {}
-}
-
-provider "aws" {
-  region  = "${var.region}"
-  version = "~> 1.16"
-}
-
 ####################################################
 # DATA SOURCE MODULES FROM OTHER TERRAFORM BACKENDS
 ####################################################
-
-//data "aws_route53_zone" "bastion_hosted_zone" {
-//  name = "${var.route53_sub_domain}"
-//}
 
 data "terraform_remote_state" "vpc" {
   backend = "s3"
