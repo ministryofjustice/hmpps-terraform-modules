4f5cff80defb9bbade3a7131ee59b9dbc8935d17
smjmoj <smjmoj@gmail.com>
2018-11-13 15:34:48 +0000

    ooutputs for both public and private ips

--- modules/natgateway/output.tf
+++ modules/natgateway/output.tf
@@ -1,3 +1,11 @@
 output natid {
   value = "${aws_nat_gateway.environment.id}"
 }
+
+output nat_public_ip {
+  value = "${aws_nat_gateway.environment.public_ip}"
+}
+
+output nat_private_ip {
+  value = "${aws_nat_gateway.environment.private_ip}"
+}
