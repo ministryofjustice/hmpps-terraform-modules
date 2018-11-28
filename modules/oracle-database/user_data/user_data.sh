04aacdd79bc297ef10f1a14af08cc993a2ca4502
smjmoj <smjmoj@gmail.com>
2018-11-28 16:15:28 +0000

    Removed develpment version / branch

--- modules/oracle-database/user_data/user_data.sh
+++ modules/oracle-database/user_data/user_data.sh
@@ -37,7 +37,6 @@ cat << EOF > ~/requirements.yml
   src: singleplatform-eng.users
 - name: oracle-db
   src: https://github.com/ministryofjustice/hmpps-delius-core-oracledb-bootstrap.git
-  version: feature/oracle-db-vars_ssm
 EOF
 
 /usr/bin/curl -o ~/users.yml https://raw.githubusercontent.com/ministryofjustice/hmpps-delius-ansible/master/group_vars/${bastion_inventory}.yml
