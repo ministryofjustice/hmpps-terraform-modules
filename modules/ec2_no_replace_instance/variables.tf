2e1793f9c87cf16e17e1053f630102aa5107f4c6
Brett Minnie <github@bdb-studios.co.uk>
2018-12-03 09:08:22 +0000

    Instance that will specified change list

---
+++ modules/ec2_no_replace_instance/variables.tf
@@ -0,0 +1,38 @@
+variable "ami_id" {}
+
+variable "instance_type" {}
+
+variable "subnet_id" {}
+
+variable "iam_instance_profile" {}
+
+variable "associate_public_ip_address" {}
+
+variable "vpc_security_group_ids" {
+  type = "list"
+}
+
+variable "key_name" {}
+
+variable "CreateSnapshot" {}
+
+variable "user_data" {}
+
+variable "app_name" {}
+
+variable "monitoring" {
+  default = true
+}
+
+variable "tags" {
+  type = "map"
+}
+
+variable "root_device_size" {
+  default = "8"
+}
+
+variable "change_list" {
+  type = "list"
+  default = ["ami", "user_data"]
+}
