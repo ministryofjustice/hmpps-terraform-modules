c28bb50efda2dcd2ad2facfa200e4015806a1f0f
Brett Minnie <github@bdb-studios.co.uk>
2018-12-06 09:51:32 +0000

    Initial centos bootstrap

--- modules/nfs-server/user_data/user_data.sh
+++ modules/nfs-server/user_data/user_data.sh
@@ -22,7 +22,38 @@ export HMPPS_ACCOUNT_ID="${account_id}"
 export HMPPS_DOMAIN="${private_domain}"
 
 cd ~
+pip install --upgrade pip
 pip install ansible
 
+cat << EOF > ~/requirements.yml
+- name: bootstrap
+  src: https://github.com/ministryofjustice/hmpps-bootstrap
+  version: centos
+- name: rsyslog
+  src: https://github.com/ministryofjustice/hmpps-rsyslog-role
+- name: elasticbeats
+  src: https://github.com/ministryofjustice/hmpps-beats-monitoring
+- name: users
+  src: singleplatform-eng.users
+EOF
+
+wget https://raw.githubusercontent.com/ministryofjustice/hmpps-delius-ansible/master/group_vars/${bastion_inventory}.yml -O users.yml
+
+cat << EOF > ~/bootstrap.yml
+---
+
+- hosts: localhost
+  vars_files:
+    - "~/users.yml"
+  roles:
+     - bootstrap
+     - rsyslog
+     - elasticbeats
+     - users
+
+EOF
+
+ansible-galaxy install -f -r ~/requirements.yml
+ansible-playbook ~/bootstrap.yml -e monitoring_host="monitoring.${private_domain}"
 
 
