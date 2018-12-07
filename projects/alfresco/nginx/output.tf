342b0ad3e308a76a5115c7ce597a2c839d08e61b
Don Nyambudzi <Don.Nyambudzi@madiro.co.uk>
2018-12-07 12:10:45 +0000

    Added ec2-nginx module (#103)

--- projects/alfresco/nginx/output.tf
+++ projects/alfresco/nginx/output.tf
@@ -19,19 +19,6 @@ output "lb_zone_id" {
   value = "${module.create_app_elb.environment_elb_zone_id}"
 }
 
-# ECS CLUSTER
-output "ecs_cluster_arn" {
-  value = "${module.ecs_cluster.ecs_cluster_arn}"
-}
-
-output "ecs_cluster_id" {
-  value = "${module.ecs_cluster.ecs_cluster_id}"
-}
-
-output "ecs_cluster_name" {
-  value = "${module.ecs_cluster.ecs_cluster_name}"
-}
-
 # LOG GROUPS
 output "loggroup_arn" {
   value = "${module.create_loggroup.loggroup_arn}"
@@ -39,32 +26,6 @@ output "loggroup_arn" {
 
 output "loggroup_name" {
   value = "${module.create_loggroup.loggroup_name}"
-}
-
-# TASK DEFINITION
-output "task_definition_arn" {
-  value = "${module.app_task_definition.task_definition_arn}"
-}
-
-output "task_definition_family" {
-  value = "${module.app_task_definition.task_definition_family}"
-}
-
-output "task_definition_revision" {
-  value = "${module.app_task_definition.task_definition_revision}"
-}
-
-# ECS SERVICE
-output "ecs_service_id" {
-  value = "${module.app_service.ecs_service_id}"
-}
-
-output "ecs_service_name" {
-  value = "${module.app_service.ecs_service_name}"
-}
-
-output "ecs_service_cluster" {
-  value = "${module.app_service.ecs_service_cluster}"
 }
 
 # Launch config
