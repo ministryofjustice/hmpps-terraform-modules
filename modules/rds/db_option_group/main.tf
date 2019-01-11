455bf98bab6127032055ea34ed177a19675ed6b9
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2019-01-11 11:10:01 +0000

    Removed lifecylce rule for db option group

--- modules/rds/db_option_group/main.tf
+++ modules/rds/db_option_group/main.tf
@@ -5,12 +5,6 @@ resource "aws_db_option_group" "this" {
   option_group_description = "${var.option_group_description == "" ? format("Option group for %s", var.identifier) : var.option_group_description}"
   engine_name              = "${var.engine_name}"
   major_engine_version     = "${var.major_engine_version}"
-
-  option = ["${var.options}"]
-
-  tags = "${merge(var.tags, map("Name", format("%s", var.identifier)))}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  option                   = ["${var.options}"]
+  tags                     = "${merge(var.tags, map("Name", format("%s", var.identifier)))}"
 }
