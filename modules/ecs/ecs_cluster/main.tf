71898ae55dd445ed58f3529ee524d6280bff99d8
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2019-03-14 16:52:24 +0000

    Added tags variable to ecs cluster with a default value

--- modules/ecs/ecs_cluster/main.tf
+++ modules/ecs/ecs_cluster/main.tf
@@ -1,3 +1,8 @@
 resource "aws_ecs_cluster" "environment" {
   name = "${var.cluster_name}-ecs-cluster"
+
+  tags = "${merge(
+    var.tags,
+    map("Name", "${var.cluster_name}-ecs-cluster")
+  )}"
 }
