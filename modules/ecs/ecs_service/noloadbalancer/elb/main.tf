43340eb448ce39bfe11580d2cc4ad61eea693535
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2019-03-22 12:48:42 +0000

    Added ecs service nolb v1

--- modules/ecs/ecs_service/noloadbalancer/elb/main.tf
+++ modules/ecs/ecs_service/noloadbalancer/elb/main.tf
@@ -3,6 +3,5 @@ resource "aws_ecs_service" "environment" {
   cluster                            = "${var.clustername}"
   task_definition                    = "${var.task_definition_family}:${max("${var.task_definition_revision}", "${var.current_task_definition_version}")}"
   desired_count                      = "${var.service_desired_count}"
-  iam_role                           = "${var.ecs_service_role}"
   deployment_minimum_healthy_percent = "${var.deployment_minimum_healthy_percent}"
 }
