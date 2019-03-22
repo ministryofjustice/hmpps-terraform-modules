cd29b99f22484d0a1921e263c2422453bc2bcee7
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2019-03-22 12:41:51 +0000

    Added ecs service nolb

---
+++ modules/ecs/ecs_service/noloadbalancer/elb/variables.tf
@@ -0,0 +1,13 @@
+variable "servicename" {}
+variable "clustername" {}
+
+variable "task_definition_family" {}
+variable "task_definition_revision" {}
+variable "service_desired_count" {}
+variable "ecs_service_role" {}
+
+variable "deployment_minimum_healthy_percent" {
+  default = "50"
+}
+
+variable "current_task_definition_version" {}
