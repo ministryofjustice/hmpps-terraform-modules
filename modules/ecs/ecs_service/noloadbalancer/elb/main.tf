ced75b4c71f76367c99db696d2f19e2c2f853385
Don Nyambudzi <Don.Nyambudzi@madiro.co.uk>
2019-03-22 13:40:11 +0000

    Issue 137 (#141)
    * Added ecs service nolb
    
    * Added ecs service nolb v1

---
+++ modules/ecs/ecs_service/noloadbalancer/elb/main.tf
@@ -0,0 +1,7 @@
+resource "aws_ecs_service" "environment" {
+  name                               = "${var.servicename}-ecs-svc"
+  cluster                            = "${var.clustername}"
+  task_definition                    = "${var.task_definition_family}:${max("${var.task_definition_revision}", "${var.current_task_definition_version}")}"
+  desired_count                      = "${var.service_desired_count}"
+  deployment_minimum_healthy_percent = "${var.deployment_minimum_healthy_percent}"
+}
