c3988b56bc021f4ad2bd2c8eefa593919ebabf57
Brett Minnie <github@bdb-studios.co.uk>
2019-03-25 10:25:54 +0000

    Ensure we encrypt our s3 backups (#143)

--- components/monitoring/elasticsearch-backup-bucket/main.tf
+++ components/monitoring/elasticsearch-backup-bucket/main.tf
@@ -1,8 +1,23 @@
+resource "aws_kms_key" "s3_bucket_encryption_key" {
+  description             = "Ensure backups at rest are encrypted"
+  deletion_window_in_days = 10
+}
+
+
 resource "aws_s3_bucket" "elasticsearch_backup_bucket" {
   bucket  = "${var.bucket_name}"
 
   acl     = "${var.acl}"
   tags    = "${var.tags}"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.s3_bucket_encryption_key.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_policy" "elasticsearch_backup_bucket_policy" {
