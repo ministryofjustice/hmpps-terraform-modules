f55d6a6583be3c14ba7dedb28d10301c615fc68c
eddie nyambudzi <e.nyambudzi@googlemail.com>
2019-04-11 18:09:47 +0200

    adding extra tag for persitant eips v2

--- modules/natgateway/main.tf
+++ modules/natgateway/main.tf
@@ -1,6 +1,6 @@
 resource "aws_eip" "environment" {
   vpc  = true
-  tags = "${merge(var.tags, map("Name", "${var.az}-nat-gateway"), map("Do  not delete", "true")}"
+  tags = "${merge(var.tags, map("Name", "${var.az}-nat-gateway"), map("Do-not-delete", "true")}"
   lifecycle {
     prevent_destroy = true
   }
