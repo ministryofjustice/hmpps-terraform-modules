89b087a1440b12e79bd7f58b5c467ac180c9556d
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2019-05-30 10:53:01 +0100

    Added s3bucket module with kms and no versioning Disabled versioning on Alfresco config bucket

---
+++ modules/s3bucket/s3bucket_kms_encryption/variables.tf
@@ -0,0 +1,17 @@
+variable "s3_bucket_name" {}
+
+variable "acl" {
+  default = "private"
+}
+
+variable "tags" {
+  type = "map"
+}
+
+variable "kms_master_key_id" {
+  description = "kms master key id"
+}
+
+variable "sse_algorithm" {
+  default = "aws:kms"
+}
