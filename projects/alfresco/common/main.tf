f69f49aaf78d3022ff5ca4e8ceed12fb92f0c1f9
Don Nyambudzi <Don.Nyambudzi@madiro.co.uk>
2019-06-17 12:00:29 +0100

    Added outbound https (#174)

--- projects/alfresco/common/main.tf
+++ projects/alfresco/common/main.tf
@@ -41,6 +41,18 @@ resource "aws_security_group_rule" "http" {
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   description       = "${local.common_name}-http"
+  count             = "${var.sg_create_outbound_web_rules}"
+}
+
+resource "aws_security_group_rule" "https" {
+  security_group_id = "${aws_security_group.vpc-sg-outbound.id}"
+  type              = "egress"
+  from_port         = "443"
+  to_port           = "443"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "${local.common_name}-https"
+  count             = "${var.sg_create_outbound_web_rules}"
 }
 
 # #-------------------------------------------
