4995941bffb1eb201618ddcceb5b0b63cf43c35e
eddie nyambudzi <e.nyambudzi@googlemail.com>
2020-06-13 01:01:03 +0200

    adding notifications for stop/start

--- modules/auto-start/auto-stop-notify/lambda/auto-stop-notification.js
+++ modules/auto-start/auto-stop-notify/lambda/auto-stop-notification.js
@@ -4,7 +4,6 @@ var util = require('util');
 exports.handler = function(event, context) {
     console.log(JSON.stringify(event, null, 2));
 
-
         const environment = process.env.ENVIRONMENT_TYPE;
         var heading = "EC2 Instance Auto-Stop Notification";
         var bodytext = "Please be advised EC2 Instances are scheduled to stop in approximately 60 mins.";
@@ -12,12 +11,28 @@ exports.handler = function(event, context) {
         var url_path = process.env.URL_PATH;
         var icon_emoji=":sign-warning:";
         const tagged_user = process.env.TAGGED_USER;
+        var action = event.action;
+
+        if (action=='start' )
+           bodytext="EC2 Instances Starting Up Now";
+
+        if (action=='start' )
+           icon_emoji=":white_check_mark:";
+
+        if (action=='start' )
+           heading="EC2 Instance Auto-Start Notification";
+
+        if (action=='stop' )
+            bodytext="EC2 Instances Stopping Now!";
+
+        if (action=='stop' )
+           icon_emoji=":stop3:";
 
             console.log("Slack channel: " + channel);
 
                var postData = {
                        "channel": "# " + channel,
-                       "username": "AWS SNS via Lambda :: EC2 Auto-stop notification",
+                       "username": "AWS SNS via Lambda :: EC2 Auto-Stop/Start",
                        "text": "**************************************************************************************************"
                        + "\nInfo: "  + heading
                        + "\nDetails: "  + bodytext
