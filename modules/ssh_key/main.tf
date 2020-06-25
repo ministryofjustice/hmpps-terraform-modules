ed2b63c3209a18391e49e9b1ededaff29fe1f6d9
smjmoj <smjmoj@gmail.com>
2020-06-25 18:25:59 +0100

    terraform 0.12upgrade of 'ssh_key'

--- modules/ssh_key/main.tf
+++ modules/ssh_key/main.tf
@@ -1,9 +1,10 @@
 resource "tls_private_key" "environment" {
   algorithm = "RSA"
-  rsa_bits  = "${var.rsa_bits}"
+  rsa_bits  = var.rsa_bits
 }
 
 resource "aws_key_pair" "environment" {
   key_name   = "${var.keyname}-ssh-key"
-  public_key = "${tls_private_key.environment.public_key_openssh}"
+  public_key = tls_private_key.environment.public_key_openssh
 }
+
--- modules/ssh_key/output.tf
+++ modules/ssh_key/output.tf
@@ -1,11 +1,12 @@
 output "public_key_openssh" {
-  value = "${tls_private_key.environment.public_key_openssh}"
+  value = tls_private_key.environment.public_key_openssh
 }
 
 output "private_key_pem" {
-  value = "${tls_private_key.environment.private_key_pem}"
+  value = tls_private_key.environment.private_key_pem
 }
 
 output "deployer_key" {
-  value = "${aws_key_pair.environment.key_name}"
+  value = aws_key_pair.environment.key_name
 }
+
