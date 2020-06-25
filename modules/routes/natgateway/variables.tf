9af3f8df351468e8bc396633d8e9fae96304549b
smjmoj <smjmoj@gmail.com>
2020-06-25 16:15:26 +0100

    Changes made by the terraform 0.12upgrade tool

--- modules/routes/natgateway/variables.tf
+++ modules/routes/natgateway/variables.tf
@@ -1,11 +1,12 @@
 variable "route_table_id" {
-  type = "list"
+  type = list(string)
 }
 
 variable "nat_gateway_id" {
-  type = "list"
+  type = list(string)
 }
 
 variable "destination_cidr_block" {
-  type = "list"
+  type = list(string)
 }
+
