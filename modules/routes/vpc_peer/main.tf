62c2c634ef52449465ba9a54401b1e0bfadd443d
marcus.aspin <maspin@unilink.com>
2020-08-12 16:30:44 +0100

    Fix count condition om aws_route

--- modules/routes/vpc_peer/main.tf
+++ modules/routes/vpc_peer/main.tf
@@ -1,5 +1,5 @@
 resource "aws_route" "environment" {
-  count                     = length(var.route_table_id) * var.create ? 1 : 0
+  count                     = length(var.route_table_id) * var.create > 0 ? 1 : 0
   route_table_id            = element(var.route_table_id, count.index)
   destination_cidr_block    = var.destination_cidr_block
   vpc_peering_connection_id = var.vpc_peer_id
