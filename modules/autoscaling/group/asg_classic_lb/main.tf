0222385a790d31af0d93165e8545fffa70233509
Martin Spasov <martin.spasov@solirius.com>
2020-10-26 14:12:47 +0000

    NOJIRA change autoscaling group tags

--- modules/autoscaling/group/asg_classic_lb/main.tf
+++ modules/autoscaling/group/asg_classic_lb/main.tf
@@ -2,16 +2,6 @@ locals {
   tags = var.tags
 }
 
-data "null_data_source" "tags" {
-  count = length(keys(local.tags))
-
-  inputs = {
-    key                 = element(keys(local.tags), count.index)
-    value               = element(values(local.tags), count.index)
-    propagate_at_launch = true
-  }
-}
-
 resource "aws_autoscaling_group" "environment" {
   name                 = var.asg_name
   vpc_zone_identifier  = var.subnet_ids
@@ -26,12 +16,13 @@ resource "aws_autoscaling_group" "environment" {
   }
 
   tags = [
-    data.null_data_source.tags.*.outputs,
-    {
-      key                 = "Name"
-      value               = "${var.asg_name}-asg"
+    for key, value in merge(var.tags, {
+      "Name" = "${var.asg_name}-asg"
+    }) : {
+      key                 = key
+      value               = value
       propagate_at_launch = true
-    },
+    }
   ]
 }
 
