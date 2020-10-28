829ce301763d410a1298de761f811b6c5592495d
smjmoj <smjmoj@gmail.com>
2020-10-28 10:44:52 +0000

    testing output change

--- modules/vpc_peering/peering_connection_accepter/output.tf
+++ modules/vpc_peering/peering_connection_accepter/output.tf
@@ -1,16 +1,15 @@
 output "id" {
-  value = aws_vpc_peering_connection_accepter.peer.*.id
+  value = aws_vpc_peering_connection_accepter.peer.id
 }
 
 output "peer_vpc_id" {
-  value = aws_vpc_peering_connection_accepter.peer.*.peer_vpc_id
+  value = aws_vpc_peering_connection_accepter.peer.peer_vpc_id
 }
 
 output "accepter" {
-  value = aws_vpc_peering_connection_accepter.peer.*.accepter
+  value = aws_vpc_peering_connection_accepter.peer.accepter
 }
 
 output "requester " {
-  value = aws_vpc_peering_connection_accepter.peer.*.requester
+  value = aws_vpc_peering_connection_accepter.peer.requester
 }
-
