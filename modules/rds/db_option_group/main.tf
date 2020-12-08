d1b3982e116743c4102b7618ae007c853bd93003
Chris Kinsella <chris.kinsella@helkinsoft.com>
2020-12-08 10:41:33 +0000

    removed []

--- modules/rds/db_option_group/main.tf
+++ modules/rds/db_option_group/main.tf
@@ -5,6 +5,6 @@ resource "aws_db_option_group" "this" {
   option_group_description = var.option_group_description == "" ? format("Option group for %s", var.identifier) : var.option_group_description
   engine_name              = var.engine_name
   major_engine_version     = var.major_engine_version
-  option                   = [var.options]
+  option                   = var.options
   tags                     = merge(var.tags, map("Name", format("%s", var.identifier)))
 }
